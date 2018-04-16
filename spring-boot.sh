86f00f805c90254047ec7136bfe588fc91dd0b57
lipeng <lipengab@chanjet.com>
2018-04-16 10:25:24 +0800

    add eureka-client eureka-service

--- spring-boot.sh
+++ spring-boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APP_NAME=$1
 nohup java $JAVA_OPTS -Dfile.encoding=UTF-8 -Xms64m -Xmx128m -jar $APP_NAME --server.port=8001 > /dev/null 2>&1 &
-nohup java $JAVA_OPTS -Dfile.encoding=UTF-8 -Xms64m -Xmx128m -jar $APP_NAME --server.port=8002 > /dev/null 2>&1 &
+nohup java $JAVA_OPTS -Dfile.encoding=UTF-8 -Xms64m -Xmx128m -jar $APP_NAME --server.port=9001 > /dev/null 2>&1 &
